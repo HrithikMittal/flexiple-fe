15d9304e2f9f7310e2a9505def86b3d3d64276ca
Adhikansh Mittal <adhikanshmittal@gmail.com>
2023-05-14 08:35:28 +0530

    url for production

--- next.config.js
+++ next.config.js
@@ -4,8 +4,7 @@ const nextConfig = {
     domains: ["xsgames.co", "cdn-icons-png.flaticon.com"],
   },
   env: {
-    // REACT_APP_BASE_URL: "https://flexiple-be.onrender.com",
-    REACT_APP_BASE_URL: "http://localhost:4000",
+    REACT_APP_BASE_URL: "https://flexiple-be.onrender.com",
   },
 };
 
